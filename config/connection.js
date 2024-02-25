7eca711f5ae80d202708f61c67a15754b5901ca0
vonmilly <dmillgit@gmail.com>
2024-02-25 15:49:44 -0500

    modified connection 2

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,17 @@ require('dotenv').config();
 
 // Use environment variables to connect to database
 const sequelize = new Sequelize(
-  process.env.JAWSDB_URL || null,
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'mysql'
-  }
-);
-
+    process.env.JAWSDB_URL || '',
+    {
+      dialect: 'mysql',
+      protocol: 'mysql',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    }
+  );
+  
 module.exports = sequelize;
